5d56653a39b0cde5e86d6466579af06cf9258aa6
neznajko <neznajko@yahoo.com>
2025-01-25 08:22:26 +0200

    fyorst commit

---
+++ chachachat/src/main/java/com/chachachat/ChachachatApplication.java
@@ -0,0 +1,13 @@
+package com.chachachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChachachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChachachatApplication.class, args);
+	}
+
+}
---
+++ chachachat/src/test/java/com/chachachat/ChachachatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chachachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChachachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
