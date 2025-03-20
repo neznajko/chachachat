8dd9f280d8ea9fe6c3d0f7ccba8a9ea9fbb3faab
neznajko <neznajko@yahoo.com>
2025-03-20 12:23:16 +0200

    図書館

--- chachachat/src/main/java/com/chachachat/WebSocketConfig.java
+++ chachachat/src/main/java/com/chachachat/WebSocketConfig.java
@@ -27,4 +27,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 // completely random time slots, independently. Th-e college offers exactly 3 statistics
 // courses. What is the probability that 2 or more o=f the statistics courses are in the
 // same time slot?
+// We can r==epresent an elementary event as (i,j,k), meaning the first course is in the
+// ith slot, t--he second course is in the jth slot and the third course in the kth slot,
+// so the size of the sa==mple space is #(Omega) = 10*10*10, let E is the event that all
+// i,j,k are d=i=f=f=e=r====e=n=t so #(E) = 10*9*8, and the desired probability is P(!E):
+// P(!E) = 1 - P(E) = 1 - 10*9*8/10*10*10 = 10*(100 - 72)/1000 = 28/100 = 28%
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
